9deca0794b9cee8117dc43a009c6bccc1c8f0506
m.lobanov <m.lobanov@bars-open.ru>
2026-02-27 20:34:26 +0300

    Аутентификация на фронте.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="app">
-    <Sidebar />
-    <PatientListPanel />
-    <div class="main" role="main">
-      <MainHeader />
+    <template v-if="!isLoginPage">
+      <Sidebar />
+      <PatientListPanel />
+    </template>
+    <div class="main" :class="{ 'full-width': isLoginPage }" role="main">
+      <MainHeader v-if="!isLoginPage" />
       <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script>
-import Sidebar from './components/Sidebar/Sidebar.vue'
-import PatientListPanel from './components/PatientListPanel/PatientListPanel.vue'
-import MainHeader from './components/MainHeader.vue'
+import Sidebar from './components/Sidebar/Sidebar.vue';
+import PatientListPanel from './components/PatientListPanel/PatientListPanel.vue';
+import MainHeader from './components/MainHeader.vue';
 
 export default {
   name: 'App',
@@ -20,6 +22,12 @@ export default {
     Sidebar,
     PatientListPanel,
     MainHeader
+  },
+  computed: {
+    isLoginPage() {
+      return this.$route.meta.isLoginPage === true;
+    }
   }
 }
 </script>
+
