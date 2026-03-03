4f10805292e03258153bbf73b3a9d64810b5f51c
m.lobanov <m.lobanov@bars-open.ru>
2026-03-03 16:54:11 +0300

    fix: Бесконечный скролл ломается при дублях в бд

--- frontend/src/api/patients.js
+++ frontend/src/api/patients.js
@@ -5,11 +5,9 @@ import { PATIENTS_PER_PAGE } from '@/utils/constants'
 export const patientApi = {
     getAll(page = 1, itemsPerPage = PATIENTS_PER_PAGE, filters = {}, order = {}) {
         const params = createPaginationParams(page, itemsPerPage, filters)
-
         Object.entries(order).forEach(([field, direction]) => {
             params[`order[${field}]`] = direction
         })
-
         return apiClient.get('/patients', { params }).then(extractData)
     },
 
