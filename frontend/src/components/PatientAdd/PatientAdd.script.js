009d685b1f712eed92f4bf408f3b4354595b6ad0
m.lobanov <m.lobanov@bars-open.ru>
2026-04-14 12:58:13 +0300

    front new Подготовка шаблона

--- frontend/src/components/PatientAdd/PatientAdd.script.js
+++ frontend/src/components/PatientAdd/PatientAdd.script.js
@@ -1,6 +1,26 @@
 export default {
     name: 'PatientAdd',
     data() {
-        return {};
+        return {
+            patient: {
+                lastname: '',
+                firstname: '',
+                secondName: '',
+                birthday: '',
+                sex: 0,
+                smsPhone: '',
+                address: '',
+                passport: '',
+                snils: '',
+                healthInsurance: '',
+                comment: ''
+            }
+        };
     },
+    methods: {
+        submitForm() {
+            // временно
+            console.log('Форма отправлена', this.patient);
+        }
+    }
 };
