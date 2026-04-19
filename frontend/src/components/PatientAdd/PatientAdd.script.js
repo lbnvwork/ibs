81549af86998ff3f0eeb92a19edb188592a25338
m.lobanov <m.lobanov@bars-open.ru>
2026-04-19 15:20:20 +0300

    front new валидация и маскирование

--- frontend/src/components/PatientAdd/PatientAdd.script.js
+++ frontend/src/components/PatientAdd/PatientAdd.script.js
@@ -1,6 +1,7 @@
 import { hospitalApi } from '@/api/hospitals';
 import { patientApi } from '@/api/patients';
-import { TREATMENT_ADD } from '@/router/paths';
+import { formatPhone, formatPassport, formatSnils } from '@/utils/formatters';
+import { isValidPhone, isValidPassport, isValidSnils } from '@/utils/validators';
 
 export default {
     name: 'PatientAdd',
@@ -34,13 +35,46 @@ export default {
             this.loadingHospitals = true;
             try {
                 this.hospitals = await hospitalApi.getAll();
-            } catch (error) {
-                console.error('Ошибка при загрузке больниц:', error);
+            } catch (err) {
+                console.error('Ошибка при загрузке больниц:', err);
             } finally {
                 this.loadingHospitals = false;
             }
         },
+        formatPhoneField() {
+            this.patient.smsPhone = formatPhone(this.patient.smsPhone);
+        },
+        formatPassportField() {
+            this.patient.passport = formatPassport(this.patient.passport);
+        },
+        formatSnilsField() {
+            this.patient.snils = formatSnils(this.patient.snils);
+        },
+        validateForm() {
+            if (!this.patient.lastname) return 'Фамилия обязательна';
+            if (!this.patient.firstname) return 'Имя обязательно';
+            if (!this.patient.birthday) return 'Дата рождения обязательна';
+            if (!this.patient.smsPhone) return 'Телефон обязателен';
+            if (!this.patient.hospitalId) return 'Больница обязательна';
+
+            if (!isValidPhone(this.patient.smsPhone)) {
+                return 'Телефон должен содержать 11 цифр (8 или 7) в формате 8(XXX)XXX-XX-XX';
+            }
+            if (this.patient.passport && !isValidPassport(this.patient.passport)) {
+                return 'Паспорт должен быть в формате XXXX XXXXXX (4 и 6 цифр)';
+            }
+            if (this.patient.snils && !isValidSnils(this.patient.snils)) {
+                return 'СНИЛС должен содержать 11 цифр в формате XXX-XXX-XXX XX';
+            }
+            return null;
+        },
         async submitForm() {
+            const validationError = this.validateForm();
+            if (validationError) {
+                this.error = validationError;
+                return;
+            }
+
             this.loading = true;
             this.error = null;
 
@@ -56,19 +90,23 @@ export default {
                     passport: this.patient.passport,
                     snils: this.patient.snils,
                     healthInsurance: this.patient.healthInsurance,
-                    comment: this.patient.comment   ,
+                    comment: this.patient.comment,
                     hospital: `/api/hospitals/${this.patient.hospitalId}`,
                 };
 
-                console.log('Отправляемые данные:', patientData);
                 const createdPatient = await patientApi.create(patientData);
-                this.$router.push(TREATMENT_ADD(createdPatient.id));
+                this.$router.push(`/patient/${createdPatient.id}/treatment/add`);
             } catch (err) {
                 console.error(err);
-                this.error = err.response?.data?.detail || 'Ошибка при сохранении пациента';
+                const detail = err.response?.data?.detail;
+                if (detail && detail.includes('duplicate key value violates unique constraint')) {
+                    this.error = 'Пациент с такими данными (возможно, телефон) уже существует.';
+                } else {
+                    this.error = detail || 'Ошибка при сохранении пациента';
+                }
             } finally {
                 this.loading = false;
             }
-        }
-    }
+        },
+    },
 };
