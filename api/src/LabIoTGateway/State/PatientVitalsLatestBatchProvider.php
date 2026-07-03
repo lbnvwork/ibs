0b72fa66caec785f7cf10689d1935991e916b6ab
m.lobanov <m.lobanov@bars-open.ru>
2026-07-03 15:30:51 +0300

    2.23 back подготовка метода для получения витальных показателей для пациентов

---
+++ api/src/LabIoTGateway/State/PatientVitalsLatestBatchProvider.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace App\LabIoTGateway\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use App\Entity\PatientVitalsLatest;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class PatientVitalsLatestBatchProvider implements ProviderInterface
+{
+    public function __construct(private EntityManagerInterface $em) {}
+
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): iterable
+    {
+        $request = $context['request'] ?? null;
+        $qb = $this->em->getRepository(PatientVitalsLatest::class)->createQueryBuilder('v');
+
+        if ($request instanceof Request) {
+            $patientIds = $request->query->all('patient_id');
+            if (!empty($patientIds)) {
+                // Приводим значения к целым числам
+                $patientIds = array_map('intval', $patientIds);
+                $qb->where($qb->expr()->in('v.patient', ':ids'))
+                   ->setParameter('ids', $patientIds);
+            }
+        }
+
+        return $qb->getQuery()->getResult();
+    }
+}
